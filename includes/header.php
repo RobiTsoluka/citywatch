bb1efa775e3423aaf2ffe73064d8407ec478b4e2
Robi Tsoluka <robitsoluka5@gmail.com>
2026-06-01 23:40:30 +0200

    ajout de la structuration des dossier et de la barre de navigation

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/./css/main.css">
+    <title>NAVBAR</title>
+</head>
+<body>
+
+<nav class="navbar">
+    <a class="link-logo" href="../index.php">
+        <div class="logo">
+            <img src="/assets/img/citywatch_logo.svg" alt="logo">
+            <h1>City<span>Watch</span></h1>
+        </div>
+    </a>
+    
+    <ul class="nav-center">
+        <li><a href="/index.php">Acceuil</a></li>
+        <li><a href="#">Carte</a></li>
+        <li><a href="#">Signaler</a></li>
+        <li><a href="#">Statistiques</a></li>
+        <li><a href="#">A propos</a></li>
+    </ul>
+    <ul class="nav-right">
+        <li><a href="#">Connexion</a></li>
+        <li><a href="#">S'inscrire</a></li>
+    </ul>
+</nav>
+
+
+</body>
+</html>
