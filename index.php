b978478ce7e6dedea4e2c56d7e8b1156b9257be0
Robi Tsoluka <robitsoluka5@gmail.com>
2026-06-02 11:56:14 +0200

    suite design de la page d'accueuil

--- index.php
+++ index.php
@@ -8,15 +8,73 @@
 </head>
 <body>
 
-<?php require_once "includes/header.php" ?>
 
-<div class="main-section">
-    <div class="main-section-left">
+<div>
+<?php require_once "includes/header.php" ?>
+</div>
+
+<div class="hero-section">
+    <div class="hero-section-left">
+        <div class="hero-section-left-text-haut">
+            <div class="hero-section-left-text-haut-tiret"></div>
+            <p class="hero-section-left-text-haut-description">PLATEFORME CITOYENNE &middot; TEMPS REEL</p>
+        </div>
+
+        <div class="hero-section-left-text-centre">
+            <div class="hero-section-left-text-centre-title">
+                <h1>La ville <br> qui <span>écoute</span> <br> enfin.</h1>
+            </div>
+
+            <div class="hero-section-left-text-centre-paragraphe">
+                <p>Signalez un danger, confirmez une alerte, 
+                   suivez les interventions. <br> CityWatch relie les citoyens et les autorités 
+                   instantanément.</p>
+            </div>
+            
+            
+
+
+        </div>
+
+        <div class="hero-section-left-buttons">
+            <a href="#" class="hero-section-left-button">Voir la carte</a>
+            <a href="#" class="hero-section-left-button">Faire un signalement</a>
+
+        </div>
+
+        <div class="hero-section-left-stats">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
         
 
     </div>
 
-    <div class="main-section-left">
+    <div class="hero-section-right">
+        <div>
+
+        </div>
+
+    </div>
+</div>
+
+<div class="Acceuil-cards">
+    <div>
+        <span>01</span>
+        <i></i>
+        <h3>Carte en direct</h3>
+        <p>Toutes les alertes visibles sur une carte interactive.</p>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
 
     </div>
 </div>
